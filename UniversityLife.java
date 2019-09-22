76032ad0e34a32da0ff8f8e3aaa69e1c8ee6a741
JimHunterRyan <jimmih15@gmail.com>
2019-09-22 20:09:50 +0100

    Update UniversityLife.java

--- UniversityLife.java
+++ UniversityLife.java
@@ -3,26 +3,29 @@ import java.util.concurrent.TimeUnit;
 import java.lang.*;
 import java.io.IOException;
 
+
+
 public class UniversityLife {
 
 
   public static void main(String[] args) {
 
-    Scanner reader = new Scanner(System.in);
 
+
+    opening();
+    pauseGame();
+    clearScreen();
     greeter();
+    pauseGame();
 
-    System.out.println("What is your name?");
 
-    String name = reader.nextLine();
 
-    System.out.println("Lovely to meet you " + name +"!");
 
     }
+//prints out some ascii art
+  public static void opening() {
 
-  public static void greeter() {
 
-    System.out.println("Welcome to UL: University Life");
     System.out.println("  _    _       _                    _ _           _      _  __     ");
     System.out.println(" | |  | |     (_)                  (_) |         | |    (_)/ _|    ");
     System.out.println(" | |  | |_ __  ___   _____ _ __ ___ _| |_ _   _  | |     _| |_ ___ ");
@@ -32,20 +35,48 @@ public class UniversityLife {
     System.out.println("                                           __/ |                   ");
     System.out.println("                                         |___/                    ");
 
-    long original = System.currentTimeMillis();
-    while (true) {
-        if ( System.currentTimeMillis() - original >= 3000) {
-            break;
-        }
+
+
+
+}
+  //this prints 1000 lines to clear the screen
+  public static void clearScreen() {
+
+        for(int clear = 0; clear < 1000; clear++) {
+
+        System.out.println("\b") ;
     }
 
-    for(int clear = 0; clear < 1000; clear++) {
-    System.out.println("\b") ;
-}
+  }
+  //this pauses the game for 3 seconds
+  public static void pauseGame() {
 
+      long original = System.currentTimeMillis();
 
-}
+      while (true) {
+
+          if ( System.currentTimeMillis() - original >= 3000) {
+
+              break;
+            }
+        }
+  }
+
+  //this greets the player
+  public static void greeter() {
+
+      Scanner reader = new Scanner(System.in);
+
+      System.out.println("What is your name?");
+
+      String name = reader.nextLine();
+
+      System.out.println("Lovely to meet you " + name +"!");
+  }
+
+ //this will print the game menu
  public static void menu() {
+
 
 
  }
