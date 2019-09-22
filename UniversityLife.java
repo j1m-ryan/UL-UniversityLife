8ae6cc972bd078d666bbd583ccb1d347770a8929
JimHunterRyan <jimmih15@gmail.com>
2019-09-22 17:39:37 +0100

    Update UniversityLife.java

--- UniversityLife.java
+++ UniversityLife.java
@@ -19,6 +19,15 @@ public class UniversityLife {
   public static void greeter() {
 
     System.out.println("Welcome to UL: University Life");
+    System.out.println("  _    _       _                    _ _           _      _  __     ");
+    System.out.println(" | |  | |     (_)                  (_) |         | |    (_)/ _|    ");
+    System.out.println(" | |  | |_ __  ___   _____ _ __ ___ _| |_ _   _  | |     _| |_ ___ ");
+    System.out.println(" | |  | | '_ \\| \\ \\ / / _ \\ '__/ __| | __| | | | | |    | |  _/ _ \\ ");
+    System.out.println(" | |__| | | | | |\\ V /  __/ |  \\__ \\ | |_| |_| | | |____| | ||  __/");
+    System.out.println("  \\____/|_| |_|_| \\_/ \\___|_|  |___/_|\\__|\\__, | |______|_|_| \\___|");
+    System.out.println("                                           __/ |                   ");
+    System.out.println("                                         |___/                    ");
+
     }
 
 }
